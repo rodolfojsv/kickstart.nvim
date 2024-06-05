eab17e43112b72c7fcac53b43729a54bc4a01cbd
rodolfojsv <rodolfosilva@me.com>
2024-06-05 15:29:52 -0600

    Add markdown utilities

---
+++ lua/custom/plugins/markdownpreview.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
+
+  vim.keymap.set('n', '<leader>mdp', ':MarkdownPreview<Enter>', { desc = 'Markdown Preview' }),
+}
