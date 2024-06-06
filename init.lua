89b3267b10a9da2b394464348129505871856542
rodolfojsv <rodolfosilva@me.com>
2024-06-06 15:31:29 -0600

    Adding toggle term

--- init.lua
+++ init.lua
@@ -153,7 +153,7 @@ require('lazy').setup({
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
-
+  { 'akinsho/toggleterm.nvim', version = '*', config = true },
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
