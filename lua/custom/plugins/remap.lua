3b4c0b5ef4c59565bc5554ab97c0e83bed928f6e
rodolfojsv <rodolfosilva@me.com>
2024-06-08 10:05:08 -0600

    Add remap for new vertical split

--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -3,4 +3,5 @@ return {
   vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Center after C-U' }),
   vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Center after next (n)' }),
   vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Center after next (N)' }),
+  vim.keymap.set('n', '<leader>nt', ':vsplit<Enter>', { desc = 'Vertical Split' }),
 }
