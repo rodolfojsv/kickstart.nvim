d29af5bf32c901dbc7e4e9ce831d74f41d4c5e3f
rodolfojsv <rodolfosilva@me.com>
2024-07-04 12:25:58 -0600

    change colorscheme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,26 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'godlygeek/tabular',
+  'tpope/vim-eunuch',
+  'tpope/vim-repeat',
+  'tpope/vim-surround',
+  'tpope/vim-unimpaired',
+  { 'kyazdani42/nvim-web-devicons', lazy = true },
+  { 'aymericbeaumet/vim-symlink', dependencies = { 'moll/vim-bbye' } },
+
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end,
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup()
+    end,
+  },
+}
