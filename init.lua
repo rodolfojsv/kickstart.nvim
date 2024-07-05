2c014130958d1bc56faa075cb84b0eb7b5879571
rodolfojsv <rodolfosilva@me.com>
2024-07-04 22:55:32 -0600

    added markdown tools

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
