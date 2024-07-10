acf56fb0ab92002841efeef4c462b2f25aadc51b
rodolfojsv <rodolfosilva@me.com>
2024-07-09 19:03:38 -0600

    rename hunk to harpoon/2

--- init.lua
+++ init.lua
@@ -189,7 +189,7 @@ require('lazy').setup({
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
       }
       -- visual mode
       require('which-key').register({
