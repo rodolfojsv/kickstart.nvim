1cff7767c4df6fc64445af00dfc57754f64504ab
rodolfojsv <rodolfosilva@me.com>
2024-07-14 15:21:29 -0600

    Added more to reminders

--- lua/custom/plugins/reminders.lua
+++ lua/custom/plugins/reminders.lua
@@ -2,6 +2,7 @@ return {
   require('reminders').setup { directory_path = '/Users/rodolfojosesilvavazquez/Development/reminders' },
   vim.keymap.set('n', '<leader>rme', ':RemindMeEvery ', { desc = '[R]emind [M]e [E]very and type minutes' }),
   vim.keymap.set('n', '<leader>rma', ':RemindMeAt ', { desc = '[R]emind [M]e [A]t and type hour of day (24h)' }),
+  vim.keymap.set('n', '<leader>rmi', ':RemindMeIn ', { desc = '[R]emind [M]e [I]n and type minutes' }),
   vim.keymap.set('n', '<leader>rmda', ':RemindMeDailyAt ', { desc = '[R]emind [M]e [D]aily [A]t and type hour of day (24h)' }),
   vim.keymap.set('n', '<leader>rmc', ':ReminderClose<CR>', { desc = '[R]e[m]inder [C]lose' }),
   vim.keymap.set('n', '<leader>rmrz', ':ReminderRemoveAll<CR>', { desc = '[R]e[m]inder [R]emove All' }),
