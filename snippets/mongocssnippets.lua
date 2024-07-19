6252816d4335d4022e91c6c8b6e2e860a3f0a947
rodolfojsv <rodolfosilva@me.com>
2024-07-19 16:04:10 -0600

    changes to mongo cs snippet

--- snippets/mongocssnippets.lua
+++ snippets/mongocssnippets.lua
@@ -1,17 +1,12 @@
 local snippets = function()
   local ls = require 'luasnip'
   local s = ls.snippet
-  local t = ls.text_node
   local i = ls.insert_node
   local extras = require 'luasnip.extras'
   local rep = extras.rep
   local fmt = require('luasnip.extras.fmt').fmt
-  local c = ls.choice_node
 
   local cs_snips = {
-
-    -- ls.parser.parse_snippet("MongoUpdate", "UpdateDefinition<$1> = Builders<{}>.Update.{}({});")
-
     s(
       'MongoUpdate',
       fmt(
