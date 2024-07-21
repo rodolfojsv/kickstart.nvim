b4f9af378c47f9d43f76639aded4935dce19ab24
rodolfojsv <rodolfosilva@me.com>
2024-07-21 15:45:08 -0600

    changes to formatter for react

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -10,7 +10,7 @@ return {
       if opts then
         require('luasnip').config.setup(opts)
       end
-      opts.updateevents = 'TextChanged,TextChangedI'
+
       vim.tbl_map(function(type)
         require('luasnip.loaders.from_' .. type).lazy_load()
       end, { 'vscode', 'snipmate', 'lua' })
--- lua/snippets/init.lua
+++ lua/snippets/init.lua
@@ -1,4 +1,9 @@
+local ls = require 'luasnip'
 require 'snippets.basiccssnipppets'
 require 'snippets.mongocssnippets'
 
-return {}
+return {
+  ls.config.set_config {
+    updateevents = 'TextChanged,TextChangedI',
+  },
+}
