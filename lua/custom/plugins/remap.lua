82fa83c46140e0a968633cb62e5d4ab7f011b988
rodolfojsv <rodolfosilva@me.com>
2025-05-27 17:22:59 -0600

    Few changes to neovim config

--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -5,16 +5,16 @@ return {
   vim.keymap.set('n', '<leader>nt', ':vsplit<Enter>', { desc = 'Vertical Split' }),
   vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Center after next (N)' }),
   vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Center after next (n)' }),
-  vim.keymap.set('n', '<C-S-k>', ':m .-2<CR>=='), -- move line down(n)
-  vim.keymap.set('v', '<C-S-j>', ":m '>+1<CR>gv=gv"), -- move line up(v)
-  vim.keymap.set('n', '<C-S-j>', ':m .+1<CR>=='), -- move line up(n)
-  vim.keymap.set('v', '<C-S-k>', ":m '<-2<CR>gv=gv"), -- move line down(v)
+  vim.keymap.set('n', '<S-A-k>', ':m .-2<CR>=='), -- move line down(n)
+  vim.keymap.set('v', '<S-A-j>', ":m '>+1<CR>gv=gv"), -- move line up(v)
+  vim.keymap.set('n', '<S-A-j>', ':m .+1<CR>=='), -- move line up(n)
+  vim.keymap.set('v', '<S-A-k>', ":m '<-1<CR>gv=gv"), -- move line down(v)
   vim.keymap.set('n', '<leader><C-n>', '<cmd>cnext<CR>zz', { desc = 'Next item on quickfix list' }),
   vim.keymap.set('n', '<leader><C-p>', '<cmd>cprev<CR>zz', { desc = 'Previous item on quickfix list' }),
   vim.keymap.set('n', '<leader>prt', ":lua require('precognition').toggle()<CR>", { desc = '[Pr]ecognition [t]oggle' }),
   vim.keymap.set('x', '<leader>p', [["_dP]]),
-  vim.keymap.set('n', '<C-S-a>', '<C-a>'),
-  vim.keymap.set('n', '<C-S-x>', '<C-x>'),
+  vim.keymap.set('n', '<A-S-a>', '<C-a>'),
+  vim.keymap.set('n', '<A-S-x>', '<C-x>'),
   vim.keymap.set('n', '<leader><C-s>', '<cmd>:so ~/.config/nvim/lua/snippets/basiccssnipppets.lua<CR>'),
   vim.keymap.set('i', '<F18>', function()
     require('luasnip').jump(1)
