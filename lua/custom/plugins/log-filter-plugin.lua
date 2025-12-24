843e9656d08b27f932b5a276f1ee35e4f880aa8f
Silva, Rodolfo Jose (Consultant) <RodolfoJose.Silva@IGTPLC.onmicrosoft.com>
2025-12-23 18:01:58 -0600

    Add decompression commands for log filter

--- lua/custom/plugins/log-filter-plugin.lua
+++ lua/custom/plugins/log-filter-plugin.lua
@@ -3,10 +3,15 @@ return {
   -- For local development, uncomment the line below and comment out the line above
   -- dir = 'C:/Dev/logfilter.nvim',
   config = function()
-    require('log-filter').setup({
-      history_file = 'C:/temp/nvim_log_filter_history.txt',
-      max_history = 20,
-      load_entry_key = '<C-e>',
-    })
+  require('log-filter').setup {
+    history_file = 'C:/temp/nvim_log_filter_history.txt',
+    max_history = 20,
+    load_entry_key = '<C-e>',
+    decompress_commands = {
+      ['.zip'] = '"C:\\Program Files\\7-Zip\\7z.exe" x -so "%s"',
+      ['.gz'] = '"C:\\Program Files\\7-Zip\\7z.exe" x -so "%s"', -- Optional: 7z can handle .gz too
+      ['.7z'] = '"C:\\Program Files\\7-Zip\\7z.exe" x -so "%s"', -- Optional: for .7z files
+    },
+  }
   end,
 }
