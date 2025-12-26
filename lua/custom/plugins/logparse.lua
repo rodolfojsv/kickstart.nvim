e1b102afd8b41bc2c589b62c6cdf548296c67dbd
Silva, Rodolfo Jose (Consultant) <RodolfoJose.Silva@IGTPLC.onmicrosoft.com>
2025-12-26 16:11:59 -0600

    Update lua config for logging mostly

---
+++ lua/custom/plugins/logparse.lua
@@ -0,0 +1,15 @@
+﻿return {
+  -- For local development
+  dir = 'C:/Dev/LuaNeoParse',
+  -- For GitHub version, comment out the line above and uncomment below:
+  -- 'yourusername/LuaNeoParse',
+  config = function()
+    require('LuaNeoParse').setup({
+      -- Optional: Customize keymaps
+      -- keymap_sas = '<leader>lps',
+      -- keymap_lol = '<leader>lpl',
+      -- keymap_ol = '<leader>lpo',
+      -- show_notifications = true,
+    })
+  end,
+}
