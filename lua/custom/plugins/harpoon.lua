ec562e6c2c9ca93656c7140947dc2de7c037406c
Silva, Rodolfo Jose (Consultant) <RodolfoJose.Silva@IGTPLC.onmicrosoft.com>
2025-12-26 11:41:55 -0600

    Update to harpoon 2.

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,8 +1,88 @@
 return {
   'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local harpoon = require('harpoon')
+    
+    -- Setup harpoon with multiple lists
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = true,
+      },
+      global = {
+        -- Global list configuration
+      },
+      workspace = {
+        -- Workspace-specific list configuration
+      },
+    })
 
-  vim.keymap.set('n', '<leader>ha', ':lua require("harpoon.mark").add_file()<Enter>', { desc = 'Add file to harpoon' }),
-  vim.keymap.set('n', '<leader>hm', ':lua require("harpoon.ui").toggle_quick_menu()<Enter>', { desc = 'show file list' }),
-  vim.keymap.set('n', '<C-h>', ':lua require("harpoon.ui").nav_next()<Enter>', { desc = 'go to next file' }),
-  vim.keymap.set('n', '<C-l>', ':lua require("harpoon.ui").nav_prev()<Enter>', { desc = 'go to previous file' }),
+    -- File to persist the current list choice
+    local state_file = vim.fn.stdpath('data') .. '/harpoon_list_state.txt'
+
+    -- Load the last used list from file
+    local function load_list_state()
+      local file = io.open(state_file, 'r')
+      if file then
+        local content = file:read('*all')
+        file:close()
+        if content == 'global' or content == 'workspace' then
+          return content
+        end
+      end
+      return 'workspace' -- default
+    end
+
+    -- Save the current list to file
+    local function save_list_state(list_name)
+      local file = io.open(state_file, 'w')
+      if file then
+        file:write(list_name)
+        file:close()
+      end
+    end
+
+    -- Track current active list (load from saved state)
+    _G.harpoon_current_list = load_list_state()
+
+    -- Helper function to get current list
+    local function get_current_list()
+      return harpoon:list(_G.harpoon_current_list)
+    end
+
+    -- Toggle between lists
+    vim.keymap.set('n', '<leader>hl', function()
+      if _G.harpoon_current_list == 'workspace' then
+        _G.harpoon_current_list = 'global'
+        print('Switched to global harpoon list')
+      else
+        _G.harpoon_current_list = 'workspace'
+        print('Switched to workspace harpoon list')
+      end
+      save_list_state(_G.harpoon_current_list)
+    end, { desc = 'Toggle harpoon list (workspace/global)' })
+
+    -- Add file to current list
+    vim.keymap.set('n', '<leader>ha', function()
+      get_current_list():add()
+      print('Added to ' .. _G.harpoon_current_list .. ' list')
+    end, { desc = 'Add file to harpoon' })
+
+    -- Toggle quick menu for current list
+    vim.keymap.set('n', '<leader>hm', function()
+      harpoon.ui:toggle_quick_menu(get_current_list())
+    end, { desc = 'show file list' })
+
+    -- Navigate to next file in current list
+    vim.keymap.set('n', '<C-h>', function()
+      get_current_list():next()
+    end, { desc = 'go to next file' })
+
+    -- Navigate to previous file in current list
+    vim.keymap.set('n', '<C-l>', function()
+      get_current_list():prev()
+    end, { desc = 'go to previous file' })
+  end,
 }
