b161346a109fad68b549c41cdac930984e62a1f5
Silva, Rodolfo Jose (Consultant) <RodolfoJose.Silva@IGTPLC.onmicrosoft.com>
2026-01-12 18:23:15 -0600

    Make sure it opens the SMIB folder for NeoSMIB

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -13,6 +13,36 @@ return {
   },
   cmd = "Neogit",
   keys = {
-    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    {
+      '<leader>gg',
+      function()
+        -- Check if we're in NeoSMIB and if SMIB/.git exists
+        local cwd = vim.fn.getcwd()
+        local git_dir = cwd .. '\\SMIB\\.git'
+        
+        if vim.fn.isdirectory(git_dir) == 1 then
+          -- Open neogit for the SMIB subdirectory
+          require('neogit').open({ cwd = cwd .. '\\SMIB' })
+        else
+          -- Open neogit for current directory
+          require('neogit').open()
+        end
+      end,
+      desc = 'Open Neogit',
+    },
+    {
+      '<leader>gc',
+      function()
+        local cwd = vim.fn.getcwd()
+        local git_dir = cwd .. '\\SMIB\\.git'
+        
+        if vim.fn.isdirectory(git_dir) == 1 then
+          require('neogit').open({ 'commit', cwd = cwd .. '\\SMIB' })
+        else
+          require('neogit').open({ 'commit' })
+        end
+      end,
+      desc = 'Neogit Commit',
+    },
   }
 }
