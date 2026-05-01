38686a95fa6062f991c29296c082c79c434fce50
Silva, Rodolfo Jose (Consultant) <RodolfoJose.Silva@IGTPLC.onmicrosoft.com>
2026-05-01 12:58:58 -0700

    Custom modifications for visibility

---
+++ lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,21 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+  ft = { 'markdown', 'Avante' },
+  opts = {
+    heading = {
+      icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' },
+    },
+    code = {
+      sign = false,
+      width = 'block',
+      right_pad = 1,
+    },
+    bullet = {
+      right_pad = 1,
+    },
+  },
+}
