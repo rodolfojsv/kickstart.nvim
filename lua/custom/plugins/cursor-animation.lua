eba1b0cb3401da40316a1f4eeb8e28302e984ce3
Silva, Rodolfo Jose (Consultant) <RodolfoJose.Silva@IGTPLC.onmicrosoft.com>
2026-05-14 17:29:44 -0700

    Comment out its buggy for now

--- lua/custom/plugins/cursor-animation.lua
+++ lua/custom/plugins/cursor-animation.lua
@@ -1,78 +1,78 @@
--- Cursor animation using extmarks
--- Source: https://github.com/FractalCodeRicardo/dev-config/blob/master/nvim/lua/my/cursor-animation.lua
-
-local ns = vim.api.nvim_create_namespace('CursorAnimation')
-
-vim.api.nvim_set_hl(0, 'CursorAnimationStart', { bg = '#42f56c' })
-vim.api.nvim_set_hl(0, 'CursorAnimationFade', { bg = '#2a9c45' })
-vim.api.nvim_set_hl(0, 'CursorAnimationEnd', { bg = '#000000' })
-
-local function update_mark(id, row, col, hl)
-  pcall(vim.api.nvim_buf_set_extmark, 0, ns, row, col, {
-    id = id,
-    end_row = row,
-    end_col = col + 1,
-    hl_group = hl,
-  })
-end
-
-local function create_mark(row, col, hl)
-  local id = vim.api.nvim_buf_set_extmark(0, ns, row, col, {
-    end_row = row,
-    end_col = col + 1,
-    hl_group = hl,
-  })
-  return id
-end
-
-local function delete_mark(id)
-  vim.defer_fn(function()
-    pcall(vim.api.nvim_buf_del_extmark, 0, ns, id)
-  end, 600)
-end
-
-local function is_valid_pos(row, col)
-  local line_count = vim.api.nvim_buf_line_count(0)
-  if row < 0 or row >= line_count then
-    return false
-  end
-
-  local line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
-  if line == nil then
-    return false
-  end
-
-  if col < 0 or col >= #line then
-    return false
-  end
-
-  return true
-end
-
-local function animate(id, row, col)
-  vim.defer_fn(function()
-    update_mark(id, row, col, 'CursorAnimationFade')
-  end, 200)
-
-  vim.defer_fn(function()
-    update_mark(id, row, col, 'CursorAnimationEnd')
-  end, 400)
-end
-
-vim.api.nvim_create_autocmd('CursorMoved', {
-  callback = function()
-    local pos = vim.api.nvim_win_get_cursor(0)
-    local row = pos[1] - 1
-    local col = pos[2]
-
-    if not is_valid_pos(row, col) then
-      return
-    end
-
-    local id = create_mark(row, col, 'CursorAnimationStart')
-    animate(id, row, col)
-    delete_mark(id)
-  end,
-})
-
+-- -- Cursor animation using extmarks
+-- -- Source: https://github.com/FractalCodeRicardo/dev-config/blob/master/nvim/lua/my/cursor-animation.lua
+--
+-- local ns = vim.api.nvim_create_namespace('CursorAnimation')
+--
+-- vim.api.nvim_set_hl(0, 'CursorAnimationStart', { bg = '#42f56c' })
+-- vim.api.nvim_set_hl(0, 'CursorAnimationFade', { bg = '#2a9c45' })
+-- vim.api.nvim_set_hl(0, 'CursorAnimationEnd', { bg = '#000000' })
+--
+-- local function update_mark(id, row, col, hl)
+--   pcall(vim.api.nvim_buf_set_extmark, 0, ns, row, col, {
+--     id = id,
+--     end_row = row,
+--     end_col = col + 1,
+--     hl_group = hl,
+--   })
+-- end
+--
+-- local function create_mark(row, col, hl)
+--   local id = vim.api.nvim_buf_set_extmark(0, ns, row, col, {
+--     end_row = row,
+--     end_col = col + 1,
+--     hl_group = hl,
+--   })
+--   return id
+-- end
+--
+-- local function delete_mark(id)
+--   vim.defer_fn(function()
+--     pcall(vim.api.nvim_buf_del_extmark, 0, ns, id)
+--   end, 600)
+-- end
+--
+-- local function is_valid_pos(row, col)
+--   local line_count = vim.api.nvim_buf_line_count(0)
+--   if row < 0 or row >= line_count then
+--     return false
+--   end
+--
+--   local line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
+--   if line == nil then
+--     return false
+--   end
+--
+--   if col < 0 or col >= #line then
+--     return false
+--   end
+--
+--   return true
+-- end
+--
+-- local function animate(id, row, col)
+--   vim.defer_fn(function()
+--     update_mark(id, row, col, 'CursorAnimationFade')
+--   end, 200)
+--
+--   vim.defer_fn(function()
+--     update_mark(id, row, col, 'CursorAnimationEnd')
+--   end, 400)
+-- end
+--
+-- vim.api.nvim_create_autocmd('CursorMoved', {
+--   callback = function()
+--     local pos = vim.api.nvim_win_get_cursor(0)
+--     local row = pos[1] - 1
+--     local col = pos[2]
+--
+--     if not is_valid_pos(row, col) then
+--       return
+--     end
+--
+--     local id = create_mark(row, col, 'CursorAnimationStart')
+--     animate(id, row, col)
+--     delete_mark(id)
+--   end,
+-- })
+--
 return {}
